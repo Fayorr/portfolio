4a4a4684765ff715ae71ffbbd9ac9322b0301173
fayorr <fayokunmiosho@gmail.com>
2025-07-17 09:27:17 -0400

    fixing navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,6 @@
+'use client';
 import Link from 'next/link';
+import { AlignRight, X } from 'lucide-react';
 import {
 	NavigationMenu,
 	// NavigationMenuContent,
@@ -10,34 +12,55 @@ import {
 	// NavigationMenuViewport,
 } from '@/components/ui/navigation-menu';
 import { ModeToggle } from '@/components/modeToggler';
+import { useState } from 'react';
 export function Header() {
+	const [toggle, setToggle] = useState(false);
 	return (
-		<header className='flex justify-between items-center p-4 bg-[#ffffff] dark:bg-[#0f172a]'>
+		<header className='flex justify-between items-center py-7 px-10 md:px-20 bg-[#ffffff] dark:bg-[#0f172a]  border-b-2 border-orange-500 fixed top-0 left-0 right-0 z-50'>
 			<div>
 				<Link href='/'>
-					<h1 className='text-2xl font-bold cursor-pointer'>Fayo</h1>
+					<h1 className='text-2xl font-bold uppercase cursor-pointer'>Fayo</h1>
 				</Link>
 			</div>
-			<NavigationMenu className=''>
+			{toggle ? (
+				<X
+					size={24}
+					strokeWidth={2}
+					className='block md:hidden cursor-pointer absolute top-7 right-4 z-50 text-white-500 dark:text-white'
+					onClick={() => setToggle(!toggle)}
+				/>
+			) : (
+				<AlignRight
+					className='block md:hidden cursor-pointer'
+					onClick={() => setToggle(!toggle)}
+				/>
+			)}
+			<div
+				className={` absolute md:relative top-0 right-0 w-[40%]  md:w-auto items-center gap-4 ${
+					toggle ? 'flex' : 'hidden'
+				} md:flex  flex-col md:flex-row h-screen md:h-auto bg-orange-500 md:bg-transparent`}
+			>
 				<ModeToggle />
-				<NavigationMenuList className='ml-3'>
-					<NavigationMenuItem>
-						<NavigationMenuLink asChild>
-							<Link href='/about'>About</Link>
-						</NavigationMenuLink>
-					</NavigationMenuItem>
-					<NavigationMenuItem>
-						<NavigationMenuLink asChild>
-							<Link href='/projects'>Projects</Link>
-						</NavigationMenuLink>
-					</NavigationMenuItem>
-					<NavigationMenuItem>
-						<NavigationMenuLink asChild>
-							<Link href='/contact'>Contact</Link>
-						</NavigationMenuLink>
-					</NavigationMenuItem>
-				</NavigationMenuList>
-			</NavigationMenu>
+				<NavigationMenu className=''>
+					<NavigationMenuList className='md:ml-12 flex flex-col md:flex-row gap-4  dark:text-white'>
+						<NavigationMenuItem>
+							<NavigationMenuLink asChild>
+								<Link href='/about'>About</Link>
+							</NavigationMenuLink>
+						</NavigationMenuItem>
+						<NavigationMenuItem>
+							<NavigationMenuLink asChild>
+								<Link href='/projects'>Projects</Link>
+							</NavigationMenuLink>
+						</NavigationMenuItem>
+						<NavigationMenuItem>
+							<NavigationMenuLink asChild>
+								<Link href='/contact'>Contact</Link>
+							</NavigationMenuLink>
+						</NavigationMenuItem>
+					</NavigationMenuList>
+				</NavigationMenu>
+			</div>
 		</header>
 	);
 }
