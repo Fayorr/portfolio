e3cbfced152bba24ccb035be25551742f5eb6329
fayorr <fayokunmiosho@gmail.com>
2025-07-23 08:48:35 -0400

    fixing navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,13 @@ export default function Home() {
 	return (
 		<>
 			<Header />
-			<div>
+			{/* <div>
 				<div className='flex flex-col items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[Montserrat] dark:bg-[#0f172a]'>
 					<h2>
 						Hello <span>.</span>
 					</h2>
 					<div>
-						<p>I&apos;m Jensen</p>
+						<p>I&apos;m Fayp</p>
 						<p>____________</p>
 					</div>
 					<h2>Software Developer</h2>
@@ -25,7 +25,7 @@ export default function Home() {
 				<div>
 					
 				</div>
-			</div>
+			</div> */}
 			<div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] dark:bg-[#0f172a]'>
 				<main className='flex flex-col gap-[32px] row-start-2 justify-center items-center sm:items-center font-montserrat'>
 					<h1 className='text-4xl font-bold text-center'>
