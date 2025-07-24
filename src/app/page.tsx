e3ca31eecd427182aed310ab7b8ae39d1d4ca217
fayorr <fayokunmiosho@gmail.com>
2025-07-24 18:21:04 -0400

    fixing hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import { Button } from '@/components/ui/button';
 import './globals.css';
 import { Header } from '@/components/Header';
@@ -7,8 +8,8 @@ export default function Home() {
 	return (
 		<>
 			<Header />
-			{/* <div>
-				<div className='flex flex-col items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[Montserrat] dark:bg-[#0f172a]'>
+			<div className='flex flex-col md:flex-row items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[Montserrat] dark:bg-[#0f172a]'>
+				{/* <div className='flex flex-col  items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[Montserrat] dark:bg-[#0f172a]'>
 					<h2>
 						Hello <span>.</span>
 					</h2>
@@ -21,11 +22,26 @@ export default function Home() {
 						<Button>Got a project? </Button>
 						<Button>My resume</Button>
 					</div>
+				</div> */}
+				<div className='flex justify-center items-center'>
+					{/* Light mode image */}
+					{/* <Image
+						src='/images/porfolioPicLight.png'
+						alt='Fayo'
+						width={400}
+						height={400}
+						className='block dark:hidden'
+					/> */}
+					{/* Dark mode image */}
+					{/* <Image
+						src='/images/porfolioPicDark.png'
+						alt='Fayo'
+						width={400}
+						height={400}
+						className='hidden dark:block'
+					/> */}
 				</div>
-				<div>
-					
-				</div>
-			</div> */}
+			</div>
 			<div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] dark:bg-[#0f172a]'>
 				<main className='flex flex-col gap-[32px] row-start-2 justify-center items-center sm:items-center font-montserrat'>
 					<h1 className='text-4xl font-bold text-center'>
