643a2f3be46f50469c55b1b39cf1646c5bbbd9c3
fayorr <fayokunmiosho@gmail.com>
2025-08-03 17:03:23 -0400

    added github title

--- src/components/GithubCalender.tsx
+++ src/components/GithubCalender.tsx
@@ -8,29 +8,34 @@ const years: number[] = [2025, 2024, 2023, 2022];
 export default function GithubCalender() {
 	const [year, setYear] = useState(2025);
 	return (
-		<div className='px-8 md:px-20 w-full h-full flex flex-col md:flex-row justify-between md:items-center dark:bg-[#0f172a] gap-4 md:gap-0'>
-			<div className='border border-slate-600  p-8 rounded-2xl'>
-				<GitHubCalendar
-					username='Fayorr'
-					year={year}
-					colorScheme='light'
-					fontSize={15}
-					blockSize={13}
-				/>
+		<div>
+			<div className='px-8 md:px-20 w-full h-full  dark:bg-[#0f172a] gap-4 md:gap-0 text-3xl md:text-4xl font-montserrat font-bold pb-6'>
+				<h2>My GitHub Contributions</h2>
 			</div>
-			<div className=' flex md:flex-col flex-row justify-start md:justify-between  items-center gap-4 flex-wrap '>
-				{years.map((yearValue: number, index: number) => (
-					<div
-						key={index}
-						onClick={() => setYear(yearValue)}
-						className='cursor-pointer flex flex-row  md:flex-col justify-center items-center'
-					>
-						<YearButton
-							year={yearValue}
-							currentYear={year}
-						/>
-					</div>
-				))}
+			<div className='px-8 md:px-20 w-full h-full flex flex-col md:flex-row justify-between md:items-center dark:bg-[#0f172a] gap-4 md:gap-0'>
+				<div className='border border-slate-600  p-8 rounded-2xl'>
+					<GitHubCalendar
+						username='Fayorr'
+						year={year}
+						colorScheme='light'
+						fontSize={15}
+						blockSize={13}
+					/>
+				</div>
+				<div className=' flex md:flex-col flex-row justify-start md:justify-between  items-center gap-4 flex-wrap '>
+					{years.map((yearValue: number, index: number) => (
+						<div
+							key={index}
+							onClick={() => setYear(yearValue)}
+							className='cursor-pointer flex flex-row  md:flex-col justify-center items-center'
+						>
+							<YearButton
+								year={yearValue}
+								currentYear={year}
+							/>
+						</div>
+					))}
+				</div>
 			</div>
 		</div>
 	);
