29243cc8ec01cb780ef6a2f1b69dfa364637ea71
Fayorr <fayokunmiosho@gmail.com>
2026-05-05 21:37:48 +0100

    update resume link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -48,7 +48,7 @@ export default function Home() {
 						>
 							<Link
 								target='_blank'
-								href='https://drive.google.com/file/d/195mFOlAe-7MNydv2GRbur7CwLPMUglgf/view?usp=sharing'
+								href='https://drive.google.com/file/d/1Urnl3Kq7PaDzEpar7CQxSAhMw8qDyaw2/view?usp=sharing'
 							>
 								My resume
 							</Link>
